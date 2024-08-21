db0a61dd806fe4ad69342f8c29c18ee26fe4940a
bjan816 <bjan816@aucklanduni.ac.nz>
2024-08-21 16:42:56 +0900

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,11 @@ import { AiOutlineClose } from 'react-icons/ai';
 import logo from '../../images/logo.png';
 
 const NavbarItem = ({ title, classProps }) => {
-
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
 }
 
 const Navbar = () => {
@@ -13,7 +17,12 @@ const Navbar = () => {
                 <img src={logo} alt="Logo" className='w-32 cursor-pointer' />
             </div>
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                    <NavbarItem key={item + index} title={item} />
+                ))}
+                <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+                    Login
+                </li>
             </ul>
         </nav>
     );
