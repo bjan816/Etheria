3df2a0ee77bc4b16ef0f56898516d73996b2d4b9
bjan816 <bjan816@aucklanduni.ac.nz>
2024-08-21 16:01:14 +0900

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,21 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../../images/logo.png';
+
+const NavbarItem = ({ title, classProps }) => {
+
+}
+
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+            <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+                <img src={logo} alt="Logo" className='w-32 cursor-pointer' />
+            </div>
+            <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+
+            </ul>
+        </nav>
     );
 }
 
