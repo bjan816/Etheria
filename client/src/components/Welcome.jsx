be0c093ee6e2871af40b56b50f4a62a49896aff7
bjan816 <bjan816@aucklanduni.ac.nz>
2024-08-23 13:24:16 +0900

    Adjust the card size on different screen sizes

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -66,7 +66,7 @@ const Welcome = () => {
                     </div>
                 </div>
                 <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
-                    <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism'>
+                    <div className='p-3 justify-end items-start flex-col rounded-xl h-48 sm:w-[21rem] w-[75%] my-5 eth-card white-glassmorpism'>
                         <div className='flex justify-between flex-col w-full h-full'>
                             <div className='flex justify-between items-start'>
                                 <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
