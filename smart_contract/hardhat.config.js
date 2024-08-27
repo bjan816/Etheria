0e08baabcb4f1f2d0fb7834a2b2cf0831f1e0e4e
bjan816 <bjan816@aucklanduni.ac.nz>
2024-08-27 23:12:40 +0900

    Update hardhat.config.js to use Sepolia

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,14 @@
+// https://eth-sepolia.g.alchemy.com/v2/CkJl0aTpf7_xYwKiaSzV3crtevtXjdVe
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/CkJl0aTpf7_xYwKiaSzV3crtevtXjdVe',
+      accounts: ['741f0f8f40c87b3c0e4db024a0c14711b8ba71f60c7d3cc2abff59a3dc9d3251']
+    },
+  }
 };
