09d347bea90a61d95d8549439f8afac719fd767c
bjan816 <bjan816@aucklanduni.ac.nz>
2024-08-28 21:14:28 +0900

    Connect the wallet
    Upon clicking the 'Connect wallet' button, metamask password is prompted, and the user can connect their wallet.

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -19,6 +19,8 @@ const getEthereumContract = () => {
 
 export const TransactionProvider = ({ children }) => {
 
+    const [connectedAccount, setConnectedAccount] = useState("");
+
     const checkIfWalletIsConnected = async () => {
         if (!ethereum) return alert("Please install Metamask");
 
@@ -27,12 +29,28 @@ export const TransactionProvider = ({ children }) => {
         console.log(accounts);
     }
 
+    const connectWallet = async () => {
+        try {
+            if (!ethereum) return alert("Please install Metamask");
+            
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+            setCurrentAccount(accounts[0]);
+
+
+        } catch (error) {
+            console.log(error);
+
+            throw new Error("No ethereum object.");
+        }
+    }
+
     useEffect(() => {
         checkIfWalletIsConnected();
     }, []);
 
     return (
-        <TransactionContext.Provider value={{ value: 'test' }}>
+        <TransactionContext.Provider value={{ connectWallet }}>
             {children}
         </TransactionContext.Provider>
     );
