ac99db2b21abc712b583e41d2675026d633470e7
bjan816 <bjan816@aucklanduni.ac.nz>
2024-08-29 01:15:20 +0900

    Add comment

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -9,6 +9,7 @@ module.exports = {
     sepolia: {
       url: 'https://eth-sepolia.g.alchemy.com/v2/CkJl0aTpf7_xYwKiaSzV3crtevtXjdVe',
       accounts: ['741f0f8f40c87b3c0e4db024a0c14711b8ba71f60c7d3cc2abff59a3dc9d3251']
+      // To change the account, go to Account details and show & copy the private key.
     },
   }
 };
